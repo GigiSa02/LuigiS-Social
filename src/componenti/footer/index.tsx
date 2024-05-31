fc6913f71fdb88c5fc75dc46d18874b0c21415b4
GigiSa02 <l.sannicandro@studenti.apuliadigitalmaker.it>
2024-05-31 11:48:54 +0200

    Finale

---
+++ src/componenti/footer/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './style.css';
+
+function Footer() {
+    return (
+        
+        <footer className="footer">
+            <h1>Luigi Sannicandro</h1>
+        </footer>
+    )
+}
+
+export default Footer;
